cc719efd473453378c56df736dff635731e7e81f
Johan Yngman <johan.yngman@gmail.com>
2023-07-15 21:11:03 +0200

    Tweaks

--- src/Main.hs
+++ src/Main.hs
@@ -32,15 +32,16 @@ randomString count char_list = replicateM count (randomChar char_list)
 roundStart :: String -> IO ()
 roundStart str = do
   putStrLn ("Type this: " ++ str)
-  putStr   "         : " 
+  putStr    "---------> " 
   hFlush stdout
 
-roundLoop :: String -> IO (Bool, String)
-roundLoop "" = return (True, "")
+roundLoop :: String -> IO (Bool, String, Char)
+roundLoop "" = return (True, "", '.')
 roundLoop (current:remaining) = do
   input <- getChar
+  
   if (ord input == 27) || (input /= current) then 
-    return (False, current:remaining)
+    return (False, current:remaining, input)
   else do
     putChar input
     hFlush stdout
@@ -49,22 +50,35 @@ roundLoop (current:remaining) = do
 roundSucceded :: IO ()
 roundSucceded = do
   putStrLn ""
-  putStrLn "Failed."
-
-roundFailed :: IO ()
-roundFailed = do
   putStrLn ""
-  putStrLn "Success!"
+  -- putStrLn "Success!"
+
+roundFailed :: String -> Char -> IO ()
+roundFailed "" _ = error "Something went wrong."
+roundFailed (failed_on:_) input = do
+  putStrLn ""
+  putStrLn ""
+  putStrLn ("Failed on character '" ++ [failed_on] ++ "'")
+  
+  if input `elem` charCombined then
+    putStrLn ("          You typed '" ++ [input] ++ "'")
+  else
+    putStr ""
+
+createRound :: IO ()
+createRound = do
+  str <- randomString 10 charCombined
+  roundStart str
+  (result, remaining, input) <- roundLoop str
+
+  if result then do
+    roundSucceded
+    createRound
+  else
+    roundFailed remaining input
 
 main :: IO ()
 main = do
   hSetBuffering stdin NoBuffering
   hSetEcho stdin False
-  str <- randomString 10 charCombined
-  roundStart str
-  (result, remaining) <- roundLoop str
-  if result then do
-    roundFailed
-  else
-    roundSucceded
-  hFlush stdout
+  createRound
