c0ebf3304745cee095a67419dedd86f54bdbfb58
Johan Yngman <johan.yngman@gmail.com>
2023-07-15 17:22:03 +0200

    Some input

--- src/Main.hs
+++ src/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import System.Random (randomRIO)
 import Control.Monad (replicateM)
+import System.IO
 
 charLetters :: [Char]
 charLetters = ['a'..'z']
@@ -21,6 +22,31 @@ randomChar char_list = (char_list !!) <$> randomRIO (1, length char_list - 1)
 randomString :: Int -> [Char] -> IO [Char]
 randomString count char_list = replicateM count (randomChar char_list)
 
-main :: IO ()
-main = randomString 10 charCombined >>= print
+mainLoop :: IO ()
+mainLoop = do
+  c <- getChar
+  if c == 'c' then 
+    return ()
+  else do
+    putChar c
+    mainLoop
 
+startGame :: String -> IO ()
+startGame str = do
+  putStrLn ("Type this: " ++ str)
+  putStr   "         : " 
+  hFlush stdout
+
+endGame :: IO ()
+endGame = do
+  putStrLn ""
+  putStrLn "Game Over."
+
+main :: IO ()
+main = do
+  hSetBuffering stdin NoBuffering
+  str <- randomString 10 charCombined
+  startGame str
+  mainLoop
+  endGame
+  hFlush stdout
