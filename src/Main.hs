cb595a2597bf3d698f2a8b6aaf5fd8459c27ffed
Johan Yngman <johan.yngman@gmail.com>
2023-07-15 20:04:04 +0200

    Added failure and success state

--- src/Main.hs
+++ src/Main.hs
@@ -8,14 +8,20 @@ import Data.Char
 charLetters :: [Char]
 charLetters = ['a'..'z']
 
+charLettersCaps :: [Char]
+charLettersCaps = ['A'..'Z']
+
 charNumbers :: [Char]
 charNumbers = ['0'..'9']
 
 charSpecial :: [Char]
-charSpecial = ['@', '+', '"']
+charSpecial = [ '@', '+', '"', '!', '\'', '&', '*', '?', '=', '#',
+                '<', '>', '-', '_', '|', '`', '{', '}', '[', ']', 
+                '^', '(', ')', '$', '%', '\\', '/', ';', ':', '~',
+                ',', '.']
 
 charCombined :: [Char]
-charCombined = charLetters ++ charNumbers ++ charSpecial
+charCombined = charLetters ++ charLettersCaps ++ charNumbers ++ charSpecial
 
 randomChar :: [Char] -> IO Char
 randomChar char_list = (char_list !!) <$> randomRIO (1, length char_list - 1)
@@ -23,34 +29,42 @@ randomChar char_list = (char_list !!) <$> randomRIO (1, length char_list - 1)
 randomString :: Int -> [Char] -> IO [Char]
 randomString count char_list = replicateM count (randomChar char_list)
 
-startGame :: String -> IO ()
-startGame str = do
+roundStart :: String -> IO ()
+roundStart str = do
   putStrLn ("Type this: " ++ str)
   putStr   "         : " 
   hFlush stdout
 
-mainLoop :: String -> IO ()
-mainLoop "" = return ()
-mainLoop (current:remaining) = do
+roundLoop :: String -> IO (Bool, String)
+roundLoop "" = return (True, "")
+roundLoop (current:remaining) = do
   input <- getChar
   if (ord input == 27) || (input /= current) then 
-    return ()
+    return (False, current:remaining)
   else do
     putChar input
     hFlush stdout
-    mainLoop remaining
+    roundLoop remaining
 
-endGame :: IO ()
-endGame = do
+roundSucceded :: IO ()
+roundSucceded = do
   putStrLn ""
-  putStrLn "Game Over."
+  putStrLn "Failed."
+
+roundFailed :: IO ()
+roundFailed = do
+  putStrLn ""
+  putStrLn "Success!"
 
 main :: IO ()
 main = do
   hSetBuffering stdin NoBuffering
   hSetEcho stdin False
-  str <- randomString 30 charCombined
-  startGame str
-  mainLoop str
-  endGame
+  str <- randomString 10 charCombined
+  roundStart str
+  (result, remaining) <- roundLoop str
+  if result then do
+    roundFailed
+  else
+    roundSucceded
   hFlush stdout
