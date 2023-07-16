9968762a1e9af5bff5dff9579717ff3f1cbbf509
Johan Yngman <johan.yngman@gmail.com>
2023-07-16 13:16:44 +0200

    Moved character logic to module Characters

---
+++ src/Characters.hs
@@ -0,0 +1,60 @@
+
+module Characters where
+
+import Control.Monad (replicateM)
+import System.Random (randomRIO)
+
+charLetters :: [Char]
+charLetters = ['a' .. 'z']
+
+charLettersCaps :: [Char]
+charLettersCaps = ['A' .. 'Z']
+
+charNumbers :: [Char]
+charNumbers = ['0' .. '9']
+
+charSpecial :: [Char]
+charSpecial =
+  [ '@'
+  , '+'
+  , '"'
+  , '!'
+  , '\''
+  , '&'
+  , '*'
+  , '?'
+  , '='
+  , '#'
+  , '<'
+  , '>'
+  , '-'
+  , '_'
+  , '|'
+  , '`'
+  , '{'
+  , '}'
+  , '['
+  , ']'
+  , '^'
+  , '('
+  , ')'
+  , '$'
+  , '%'
+  , '\\'
+  , '/'
+  , ';'
+  , ':'
+  , '~'
+  , ','
+  , '.'
+  ]
+
+charCombined :: [Char]
+charCombined = charLetters ++ charLettersCaps ++ charNumbers ++ charSpecial
+
+randomChar :: [Char] -> IO Char
+randomChar char_list = (char_list !!) <$> randomRIO (1, length char_list - 1)
+
+randomString :: Int -> [Char] -> IO [Char]
+randomString count char_list = replicateM count (randomChar char_list)
+
--- src/Main.hs
+++ src/Main.hs
@@ -1,63 +1,9 @@
 module Main where
 
-import Control.Monad (replicateM)
+import Characters
+
 import Data.Char
 import System.IO
-import System.Random (randomRIO)
-
-charLetters :: [Char]
-charLetters = ['a' .. 'z']
-
-charLettersCaps :: [Char]
-charLettersCaps = ['A' .. 'Z']
-
-charNumbers :: [Char]
-charNumbers = ['0' .. '9']
-
-charSpecial :: [Char]
-charSpecial =
-  [ '@'
-  , '+'
-  , '"'
-  , '!'
-  , '\''
-  , '&'
-  , '*'
-  , '?'
-  , '='
-  , '#'
-  , '<'
-  , '>'
-  , '-'
-  , '_'
-  , '|'
-  , '`'
-  , '{'
-  , '}'
-  , '['
-  , ']'
-  , '^'
-  , '('
-  , ')'
-  , '$'
-  , '%'
-  , '\\'
-  , '/'
-  , ';'
-  , ':'
-  , '~'
-  , ','
-  , '.'
-  ]
-
-charCombined :: [Char]
-charCombined = charLetters ++ charLettersCaps ++ charNumbers ++ charSpecial
-
-randomChar :: [Char] -> IO Char
-randomChar char_list = (char_list !!) <$> randomRIO (1, length char_list - 1)
-
-randomString :: Int -> [Char] -> IO [Char]
-randomString count char_list = replicateM count (randomChar char_list)
 
 roundStart :: String -> IO ()
 roundStart str = do
